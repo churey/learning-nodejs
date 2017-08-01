8b353a411eb5e987e2914162829077b6dc439e66
Baron Chu <baron.chu@oracle.com>
2017-08-01 16:02:32 +0800

    use different kinds of middleware

--- myapp/app.js
+++ myapp/app.js
@@ -1,46 +1,127 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*
+// =================================================================
+// Application-level middleware
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Express is a routing and middleware web framework that has minimal functionality of its own: An Express application is essentially a series of middleware function calls.
+var myLoggerTime = function(req, res, next) {
+	console.log('LOGGED');
+	next();
+}
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+// This example shows a middleware function with no mount path. The function is executed every time the app receives a request.
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+	console.log('app.use without path: my log time');
+	next();
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// This example shows a middleware function mounted on the /user/:id path. The function is executed for any type of HTTP request on the /user/:id path.
+app.use('/user/:id', function(req, res, next) {
+	console.log('app.use with path: user log')
+	next();
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// This example shows a route and its handler function (middleware system). The function handles GET requests to the /user/:id path.
+app.get('/user/:id', function(req, res, next) {
+	console.log('app.get: user log');
+	next();
+})
+
+
+// skip the rest of the middleware functions, use next('route')
+app.get('/book/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next route
+  if (req.params.id === '0') next('route')
+  // otherwise pass the control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // render a regular page
+  res.send('regular')
+})
+
+// this is the next route, handler for the /user/:id path, which renders a special page
+app.get('/book/:id', function (req, res, next) {
+  res.send('special')
+})
+
+*/
+
+
+/*
+// =================================================================
+// Router-level middleware
+// Router-level middleware works in the same way as application-level middleware, except it is bound to an instance of express.Router().
+// Load router-level middleware by using the router.use() and router.METHOD() functions.
+var router = express.Router();
+router.use(function(req, res, next) {
+	console.log('app.use without path: my log time');
+	next();
+})
+
+router.use('/user/:id', function(req, res, next) {
+	console.log('app.use with path: user log')
+	next();
+})
+
+router.get('/user/:id', function(req, res, next) {
+	console.log('app.get: user log');
+	next();
+})
+
+
+// skip the rest of the middleware functions, use next('route')
+router.get('/book/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next route
+  if (req.params.id === '0') next('route')
+  // otherwise pass the control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // render a regular page
+  res.send('regular')
+})
+
+// this is the next route, handler for the /user/:id path, which renders a special page
+router.get('/book/:id', function (req, res, next) {
+  res.send('special')
+})
+
+app.use('/', router);
+*/
+
+// =================================================================
+// Error-handling middleware
+// Error-handling middleware always takes four arguments
+app.use(function (err, req, res, next) {
+	console.error(err.stack)
+	res.status(500).send('Something broke!')
+})
+
+// =================================================================
+// Built-in middleware
+// The only built-in middleware function in Express is express.static. This function is based on serve-static, and is responsible for serving static assets such as HTML files, images, and so on.
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+}
+// go to APP_HOME/public to find static files
+app.use(express.static('public', options))
+// test http://localhost:3000/1.png
+
+
+// =================================================================
+// Third-party middleware
+// npm install cookie-parser
+var cookieParser = require('cookie-parser')
+// load the cookie-parsing middleware
+app.use(cookieParser())
 
 module.exports = app;
