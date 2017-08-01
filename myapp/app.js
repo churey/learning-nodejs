2ce3eaa7b4ca5dbc6f7d4dab08a3f9bb1aff6806
Baron Chu <baron.chu@oracle.com>
2017-08-01 17:08:07 +0800

    error handler

--- myapp/app.js
+++ myapp/app.js
@@ -1,46 +1,39 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get('/a_route_behind_paywall',
+  function checkIfPaidSubscriber (req, res, next) {
+    if (!req.hasPaid) {
+      // continue handling this requests
+	  next('aaa')   // error occured
+    }
+    else{
+      next();
+    }
+  }, function getPaidContent (req, res, next) {
+    PaidContent.find(function (err, doc) {
+	  console.log('**************');
+      if (err) return next(err)
+      res.json(doc)
+    })
+  })
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  
+var logError = function(err, req, res, next) {
+	console.log(err.stack);
+	console.log('log error occured');
+	next(err);
+}
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use('/a_route_behind_paywall', logError);
+app.use('/a_route_behind_paywall', function(err, req, res, next) {
+	console.log('log error occured 2');
+	next();
+});
+app.use('/a_route_behind_paywall', function(req, res, next) {
+	res.send('render page');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
