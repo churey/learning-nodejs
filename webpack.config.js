9e513b710b18f041bb9acc881b563dfe744216c5
ubuntu <baron.chu@oracle.com>
2017-08-08 18:04:15 +0800

    css reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,32 @@
-module.exports = {
-  entry: "./main.js",
-  output: {
-    filename: "bundle.js"
-  }
-}
+  const path = require('path');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const webpack = require('webpack');
+
+  module.exports = {
+    entry: {
+      app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+   module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: ['style-loader', 'css-loader']
+       }
+     ]
+   },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  };
