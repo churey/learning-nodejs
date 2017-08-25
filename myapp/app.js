4b35826558c3f735ecdef3ad0582ebd8b42cd7b3
ubuntu <baron.chu@oracle.com>
2017-08-25 13:33:30 +0800

    express middleware chain

--- myapp/app.js
+++ myapp/app.js
@@ -9,11 +9,18 @@ var myLogger1 = function(req, res, next) {
 
 var myLogger2 = function(req, res, next) {
 	console.log('LOGGED 2');
-	next();
+	//next();
 }
 
 // To load the middleware function, call app.use(), specifying the middleware function
 app.use(myLogger2);
+
+
+app.all('/', function(req, res, next) {
+	console.log('middleware filter 1');
+	next();
+});
+
 app.get('/', function(req, res) {
 	res.send('rendering home page');
 })
@@ -26,5 +33,14 @@ app.use(myLogger1);
 // middleware file    my-middleware.js
 var myMiddleware = require('./middleware/my-middleware');
 app.use(myMiddleware({'name':'Baron', 'age' : '6'}));
+
+
+// --------------------------------------------------------------//
+app.use('/middleware', myLogger1);
+app.use('/middleware', myLogger2);
+app.use('/middleware', myMiddleware({'name': 'Baron', 'age':'8'}));
+app.get('/middleware', function(req, res, next) {
+	res.end('middleware chain finished.');
+})
 
 module.exports = app;
